fa40e27eb648722389e450520389ce7777a3786d
Nils Homer <nilshomer@gmail.com>
2011-09-08 16:53:28 -0400

    * BWA and BFAST read name do not match in some rare circumstances (# of errors)

--- src/dwgsim.c
+++ src/dwgsim.c
@@ -1426,10 +1426,10 @@ void dwgsim_core(dwgsim_opt_t * opt)
                   if(ILLUMINA == opt->data_type || IONTORRENT == opt->data_type) {
                       fprintf(fpo, "@%s_%u_%u_%1u_%1u_%1u_%1u_%d:%d:%d_%d:%d:%d_%llx/%d\n", 
                               name, ext_coor[0]+1, ext_coor[1]+1, strand[0], strand[1], 0, 0,
-                              n_err[0] - n_err_first[0], 
+                              n_err[0],
                               n_sub[0] - n_sub_first[0], 
                               n_indel[0] - n_indel_first[0], 
-                              n_err[1] - n_err_first[1], 
+                              n_err[1],
                               n_sub[1] - n_sub_first[1], 
                               n_indel[1] - n_indel_first[1],
                               (long long)ii,
@@ -1447,7 +1447,8 @@ void dwgsim_core(dwgsim_opt_t * opt)
                       // annotated as read "1".
                       fprintf(fpo, "@%s_%u_%u_%1u_%1u_%1u_%1u_%d:%d:%d_%d:%d:%d_%llx/%d\n", 
                               name, ext_coor[0]+1, ext_coor[1]+1, strand[0], strand[1], 0, 0,
-                              n_err[0], n_sub[0], n_indel[0], n_err[1], n_sub[1], n_indel[1],
+                              n_err[0] - n_err_first[0], n_sub[0], n_indel[0], 
+                              n_err[1] - n_err_first[1], n_sub[1], n_indel[1],
                               (long long)ii, 2 - j);
                       //fputc('A', fpo);
                       for (i = 1; i < s[j]; ++i)
